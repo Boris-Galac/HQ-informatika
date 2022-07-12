bfd0f419aed6f7b820bff242567421e74112dd03
Boris-Galac <52317287+Boris-Galac@users.noreply.github.com>
2022-07-12 09:25:51 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('no JS yet...');
+let overlay = document.createElement('div');
+
+// nav menu
+
+const hamIcon = document.querySelector('.header__ham-btn');
+const navMenu = document.querySelector('.nav');
+
+hamIcon.addEventListener('click', (e) => {
+  let lines = [...e.currentTarget.children];
+  lines.forEach((line) => {
+    line.classList.add('active');
+  });
+  navMenu.classList.add('active');
+  overlay.classList.add('overlay');
+  document.body.append(overlay);
+  overlay.addEventListener('click', (e) => {
+    navMenu.classList.remove('active');
+    overlay.remove();
+    lines.forEach((line) => {
+      line.classList.remove('active');
+    });
+  });
+});
