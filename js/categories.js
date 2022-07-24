cc4c613a4772a913a04730f843ad1eb17d0facbb
Boris-Galac <52317287+Boris-Galac@users.noreply.github.com>
2022-07-24 23:51:01 +0200

    update

--- js/categories.js
+++ js/categories.js
@@ -1,92 +1,3 @@
-//////////  IMAGE CAROUSEL
-
-const leftArrow = document.querySelector('.hero__left');
-const rightArrow = document.querySelector('.hero__right');
-if (rightArrow) {
-  rightArrow.addEventListener('click', () => {
-    nextSlide();
-    if (auto) {
-      clearInterval(slideInterval);
-      slideInterval = setInterval(nextSlide, intervalTime);
-    }
-  });
-}
-if (leftArrow) {
-  leftArrow.addEventListener('click', () => {
-    prevSlide();
-    if (auto) {
-      clearInterval(slideInterval);
-      slideInterval = setInterval(prevSlide, intervalTime);
-    }
-  });
-}
-const carouselSlider = document.querySelector('.hero__slider');
-const images = document.querySelectorAll('.hero__slider-picture');
-
-const auto = true;
-const intervalTime = 5000;
-let slideInterval;
-
-const nextSlide = () => {
-  const current = document.querySelector('.current');
-  current.classList.remove('current');
-  if (current.nextElementSibling) {
-    current.nextElementSibling.classList.add('current');
-  } else {
-    images[0].classList.add('current');
-  }
-};
-
-const prevSlide = () => {
-  const current = document.querySelector('.current');
-  current.classList.remove('current');
-  if (current.previousElementSibling) {
-    current.previousElementSibling.classList.add('current');
-  } else {
-    images[images.length - 1].classList.add('current');
-  }
-};
-
-if (auto) {
-  slideInterval = setInterval(nextSlide, intervalTime);
-}
-
-////////// COUNTDOWN
-
-const countdown = () => {
-  const countDate = new Date('September 30, 2022 00:00:00').getTime();
-  const currentTime = new Date().getTime();
-  const gap = countDate - currentTime;
-
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const textDay = Math.floor(gap / day);
-  const textHour = Math.floor((gap % day) / hour);
-  const textMinute = Math.floor((gap % hour) / minute);
-  const textSecond = Math.floor((gap % minute) / second);
-
-  document.querySelector('.day').innerText = textDay;
-  document.querySelector('.hour').innerText = textHour;
-  document.querySelector('.minute').innerText = textMinute;
-  document.querySelector('.second').innerText = textSecond;
-};
-
-setInterval(countdown, 1000);
-
-////////// ACCORDION
-
-const accordionHeader = document.querySelectorAll('.accordion__header');
-
-accordionHeader.forEach((headerTab) => {
-  headerTab.addEventListener('click', (e) => {
-    headerTab.nextElementSibling.classList.toggle('active');
-    headerTab.lastElementChild.classList.toggle('active');
-  });
-});
-
 //////////////////////////////  SHOPPING
 
 const shopList = document.querySelector('.best-offer__wrapper');
@@ -96,7 +7,6 @@ let cart = [];
 // display items
 
 let displayItems = () => {
-  shoppingItemsArr.length = 15;
   let itemsArr = shoppingItemsArr
     .map((item) => {
       let = { category, img, product, description, price } = item;
@@ -109,7 +19,6 @@ let displayItems = () => {
           <h3 class="product__price price">${price},00 <span>€</span></h3>
         </div>
         <div class="product__right">
-          <div class="product__category">${category}</div>
            <div class="product__info">
               <div>
                   <h3 class="product__heading">${product}</h3>
@@ -124,7 +33,7 @@ let displayItems = () => {
     `;
     })
     .join('');
-  shopList.innerHTML = itemsArr;
+  categoriesWrapper.innerHTML = itemsArr;
 };
 displayItems();
 
@@ -260,7 +169,7 @@ cartIndicator();
 const removeItem = (e) => {
   const currItem = e.target.parentElement;
   let currItemHeading = currItem.firstElementChild.innerText;
-  let bestOfferItems = [...shopList.children];
+  let bestOfferItems = [...categoriesWrapper.children];
   let itemFromCart = cart.find((obj) => obj.product === currItemHeading);
   // show me items which are not in cart
   let removedItems = bestOfferItems.find((item) => {
@@ -303,28 +212,3 @@ const totalBill = () => {
     .reduce((a, b) => a + b, 0);
   total.forEach((total) => (total.innerHTML = `${x}€`));
 };
-
-////////// LOAD MORE
-
-const loadMoreBtn = document.querySelector('.best-offer__load-more');
-let startIndex = 5;
-
-loadMoreBtn.addEventListener('click', () => {
-  let products = Array.from(
-    document.querySelectorAll('.best-offer__wrapper .product')
-  );
-
-  let productsTodisplay = products.slice(startIndex, startIndex + 5);
-  productsTodisplay.forEach((product) => {
-    product.classList.add('active');
-  });
-  let items = products
-    .filter((product, index) => {
-      return index > 4;
-    })
-    .every((product) => product.classList.contains('active'));
-  if (items) {
-    loadMoreBtn.classList.add('deactivated');
-  }
-  startIndex = startIndex + 5;
-});
---
+++ js/common.js
@@ -0,0 +1,54 @@
+///////// OVERLAY
+
+let overlay = document.createElement('div');
+const createOverlay = (content) => {
+  overlay.classList.add('overlay');
+  document.body.append(overlay);
+  overlay.addEventListener('click', (e) => {
+    content.classList.remove('active');
+    overlay.remove();
+  });
+};
+
+////////// HAMBURGER MENU
+
+const hamIcon = document.querySelector('.header__ham-btn');
+const navMenu = document.querySelector('.nav');
+
+hamIcon.addEventListener('click', (e) => {
+  let lines = [...e.currentTarget.children];
+  lines.forEach((line) => {
+    line.classList.toggle('active');
+  });
+  navMenu.classList.toggle('active');
+  createOverlay(navMenu);
+  overlay.addEventListener('click', (e) => {
+    lines.forEach((line) => {
+      line.classList.remove('active');
+    });
+  });
+});
+
+////////// OPEN SHOPPING CART MENU
+
+let shoppingCartIcons = document.querySelectorAll('.cart-btn');
+const shoppingCartMenu = document.querySelector('.shopping-cart');
+
+shoppingCartIcons.forEach((cart) => {
+  cart.addEventListener('click', (e) => {
+    shoppingCartMenu.classList.toggle('active');
+    createOverlay(shoppingCartMenu);
+  });
+});
+
+////////// SEARCH BAR
+
+const searchBtn = document.querySelectorAll('.search-btn');
+const searchWindow = document.querySelector('.search');
+
+searchBtn.forEach((search) => {
+  search.addEventListener('click', (e) => {
+    searchWindow.classList.toggle('active');
+    createOverlay(searchWindow);
+  });
+});
